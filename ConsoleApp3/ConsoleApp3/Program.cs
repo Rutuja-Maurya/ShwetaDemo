aada2ced58ee2387ab2f987f5591025357004aeb
Rutuja-Maurya <rutujamaurya0707@gmail.com>
2022-04-24 14:57:29 +0530

    work done

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ValueTypeTest
+{
+
+    class ReferenceData
+    {
+
+        // Main Function
+        static void Main()
+        {
+
+            string a = "Reference";
+
+            a += "Data";
+            a = a + "Type";
+            Console.WriteLine(a);
+
+            object obj;
+            obj = 30;
+            Console.WriteLine(obj);
+
+
+            Console.WriteLine(obj.GetType());
+        }
+    }
+}
+
